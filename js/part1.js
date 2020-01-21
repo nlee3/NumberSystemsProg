e66137068d22e8c10eee559eff67d3b833336e3a
Nathan <nlee3@hwemail.com>
2020-01-20 17:49:29 -0800

    Update part1.js

--- js/part1.js
+++ js/part1.js
@@ -3,12 +3,23 @@ function ConversionPart1() {
     var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
+    var ValueOne = "";
 
+    for (int i= 0; i < String(UnsignedInt).length; i++)
+    {
+        ValueOne += ValueOne + (String)(parseInt(UnsignedInt.charAt(i))* Math.pow(UnsignedIntBaseFrom, String(UnsignedInt).length - i);
+        //ValueOne += ValueOne + (String)(parseInt(UnsignedInt.charAt(i)) * UnsignedIntBaseFrom);
+    }
 
+    var OutputValue = ValueOne;
+    var NewNumber = "";
+    while (outputValue != 0)
+    {
+      NewNumner = (OutputValue%UnsignedIntBaseTo) + NewNumber;
+      OutputValue = OutputValue/UnsignedIntBaseTo;
+    }
 
-  var outputValue = "11111111";
-
-  // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+    // Show the output on the screen
+    FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
 
 }
